c0cebd87c00d3b6fdb8c864fad41f3f20447d77a
Opeoluwa Siyanbola <opesiyanbola8991@gmail.com>
2025-01-22 23:16:45 +0100

    initial layout setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,15 @@
 import "@/styles/globals.css";
 
+import { AppSidebar } from "@/components/AppSidebar";
+import DashboardContent from "@/components/DashboardContent";
+
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 
 export const metadata: Metadata = {
   title: "Graph Visualizer",
@@ -14,7 +22,12 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body>
+        <SidebarProvider>
+          <AppSidebar />
+          <DashboardContent />
+        </SidebarProvider>
+      </body>
     </html>
   );
 }
